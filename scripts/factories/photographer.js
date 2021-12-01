436c9a95eaf8952178dc77a0aef0c759e00b0201
Marine <marine.huygens@hotmail.fr>
2021-12-01 12:28:06 +0100

    Modif scripts new repo

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,31 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
+export function photographerFactory(data) {
+  const { name, portrait, city, country, tagline, price, id } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/Photographers ID Photos/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    let article = document.createElement("article");
+    let a = document.createElement("a");
+    const img = document.createElement("img");
+    let h2 = document.createTextNode(name);
+    a.appendChild(img);
+    img.setAttribute("src", picture);
+    img.alt = "Photo profil de " + `${name}`;
+    a.title = name;
+    a.href = "photographer.html" + "?id=$" + `${id}`;
+    article.appendChild(a);
+    a.appendChild(h2);
+    const h3 = document.createElement("h3");
+    h3.textContent = city + ", " + country;
+    article.appendChild(h3);
+    const h4 = document.createElement("h4");
+    h4.textContent = tagline;
+    article.appendChild(h4);
+    const h5 = document.createElement("h5");
+    h5.textContent = price + "€/jour";
+    article.appendChild(h5);
+
+    return article;
+  }
+  return { name, picture, city, country, tagline, price, getUserCardDOM };
 }
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,88 @@
+const modal = document.getElementById("contact_modal");
+
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+  modal.style.display = "block";
 }
 
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
+  modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+  if (event.target == modal) {
     modal.style.display = "none";
+  }
+};
+
+const signForm = document.querySelector("#contact-form");
+
+signForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (formValid()) {
+    email.style.border = "none";
+    message.style.border = "none";
+    document.querySelector("#first").style.border = "none";
+    document.querySelector("#last").style.border = "none";
+    signForm.reset();
+    alert("TOP!");
+  }
+});
+
+function nameChecker(classErreur, input) {
+  const $nameError = document.querySelector(classErreur);
+  const $inputError = document.getElementById(input);
+
+  if (document.getElementById(input).value.length < 2) {
+    $nameError.style.display = "block";
+    $inputError.style.border = "2px solid rgb(141, 30, 30)";
+  } else {
+    $inputError.style.border = "2px solid green";
+    $nameError.style.display = "none";
+    return nameChecker;
+  }
+}
+
+let email = document.getElementById("email");
+function emailChecker() {
+  let regexEmail =
+    /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+  const $emailError = document.querySelector(".emailError");
+  if (email.value.trim() === "" || regexEmail.test(email.value) === false) {
+    $emailError.style.display = "block";
+    email.style.border = "2px solid rgb(141, 30, 30)";
+  } else {
+    $emailError.style.display = "none";
+    email.style.border = "2px solid green";
+    return emailChecker;
+  }
+}
+
+let message = document.getElementById("message");
+function messageChecker() {
+  const $messageError = document.querySelector(".messageError");
+  if (message.value.length < 3) {
+    $messageError.style.display = "block";
+    message.style.border = "2px solid rgb(141, 30, 30)";
+  } else {
+    $messageError.style.display = "none";
+    message.style.border = "2px solid green";
+    return messageChecker;
+  }
+}
+
+const formValid = () => {
+  nameChecker(".firstError", "first");
+  nameChecker(".lastError", "last");
+  emailChecker();
+  messageChecker();
+  const isValid =
+    nameChecker(".firstError", "first") &&
+    nameChecker(".lastError", "last") &&
+    emailChecker() &&
+    messageChecker();
+  return isValid;
+};
+
+function contactClose() {
+  modal.style.display = "none";
 }
