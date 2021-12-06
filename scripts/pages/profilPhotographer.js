0a743ee73188396c814c757d70c88c42e08eebec
Marine <marine.huygens@hotmail.fr>
2021-12-06 17:44:35 +0100

    css header profile photographer page

--- scripts/pages/profilPhotographer.js
+++ scripts/pages/profilPhotographer.js
@@ -1,23 +1,24 @@
 export default class ProfilPhotographer {
   static displayProfilPhotographer(data) {
     let photographersData = data.photographers;
-    const picture = `assets/Photographers ID Photos/${photographersData[0].portrait}`;
+
     const id = window.location.search.split("id=")[1];
     const photographers = !id
       ? photographersData
       : photographersData.filter((photographer) => photographer.id == id);
     const sectionPhotographerProfil = document.getElementsByClassName(
-      "photographer_section"
+      "photographer_section_header"
     );
-
+    const picture = `assets/Photographers ID Photos/${photographersData[0].portrait}`;
     let article = document.createElement("article");
+    article.classList.add("header-photographer-profile");
     const templatePhotographerProfil = `
                 <div class='ph-infos'>
                     <h2 class="photographe-name">${photographers[0].name}</h2>
                     <p class="localisation">${photographers[0].city}, ${photographers[0].country}</p>
                     <p class="citation">${photographers[0].tagline}</p>
                 </div>
-                <button class="contact_button" title="Contactez-moi">Contactez-moi</button>
+                <button class="contact_button ph-profile-btn" title="Contactez-moi">Contactez-moi</button>
                 <img src="${picture}" alt="${photographers[0].name}">
             `;
 
